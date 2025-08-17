cb90d8b7141abc9b9c813744f156d9b8d1d82d7f
Akkem Venkata Pavan <apawan352@gmail.com>
2025-08-17 22:17:09 +0530

    api

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000", // your backend
+  baseURL: "http://localhost:https:https://hostel-frontend-3rsc.onrender.com", // your backend
   headers: { "Content-Type": "application/json" }
 });
 
